d3364808b232e1f11bbcffbe007c87a0fb459a44
zack-ship <parsival351@gmail.com>
2025-06-21 13:04:07 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -3,29 +3,7 @@ var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
-var lyricsData = [
-  { text: "pero claro que me aterra sentir", time: 10 },
-  { text: "mas porque en tan poco tiempo", time: 15 },
-  { text: "demasiado es lo que yo siento", time: 20 },
-  { text: "corrijo, poco demasiado", time: 25 },
-  { text: "es lo que debo decir para que", time: 30 },
-  { text: "no te asustes y no te tengas que ir", time: 35 },
-  { text: "me gustas poco demasiado", time: 40 },
-  { text: "te quiero poco demasiado", time: 45 },
-  { text: "me duele poco demasido", time: 50 },
-  { text: "me aterra poco demasido", time: 55 },
-  { text: "pero que tarea tan complicada", time: 60 },
-  { text: "es ocultar algo tan bello como sentir", time: 65 },
-  { text: "todo porque realmente nos aterra...", time: 70 },
-  { text: "lo que vayan a decir", time: 75 },
-  { text: "cada ida o despedida", time: 80 },
-  { text: "cada vuelta en cada esquina", time: 85 },
-  { text: "lo que veo, lo que oigo,  lo que toco", time: 90 },
-  { text: "quizas yo soy el loco", time: 95 },
-  { text: "pero claro que me aterra sentir", time: 100 },
-  { text: "mas cuando siento", time: 105 },
-  { text: "que no te siento a ti", time: 110 },
-  
+v
   { text: "TE QUIERO Y FELIZ SANVALENTIN  ELIZABETH.", time: 115 },
 ];
 
